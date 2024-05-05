867a63457d50cd51fdff5e8692194a074ee55dd1
Raheemspb <gabibovda@gmail.com>
2024-05-05 15:43:25 +0300

    This initial commit with empty project

---
+++ Electrolux Test Work/Electrolux Test Work/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Electrolux Test Work
+//
+//  Created by Рахим Габибли on 05.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
